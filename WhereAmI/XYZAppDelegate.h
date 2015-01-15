35e445b1be6287f4516040e215cfd7a7c8a91de3
Ales Nemianionak <fealor777@gmail.com>
2015-01-15 12:41:15 +0300

    Initial Commit

---
+++ WhereAmI/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  WhereAmI
+//
+//  Created by Admin on 15.01.15.
+//  Copyright (c) 2015 PlaceHolder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WhereAmI/XYZViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZViewController.h
+//  WhereAmI
+//
+//  Created by Admin on 15.01.15.
+//  Copyright (c) 2015 PlaceHolder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZViewController : UIViewController
+
+@end
